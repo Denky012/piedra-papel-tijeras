cc5cc52c8347fed250cd288ae2bcda0da062c4a7
Denky012 <marquezperezdavid40@gmail.com>
2024-04-21 19:56:14 -0400

    Se agrego una interfaz de usuario, y ahora es interactiva la pagina

--- js/switch.js
+++ js/switch.js
@@ -1,67 +1,83 @@
-var playGame = 0,
+let playGame = 0,
   playerScore = 0,
   computerScore = 0;
+let playerSelection;
 
-function eleccion() {
-  let elegir = prompt("Escoja entre 'piedra', 'papel', 'tijeras' ");
-  let playerSelection = elegir.toLocaleLowerCase();
-
-  while (playerSelection !== "piedra" && playerSelection !== "papel" && playerSelection !== "tijeras") {
-    console.log("Elección inválida, vuelva a escoger.");
-    elegir = prompt("Escoja entre 'piedra', 'papel', 'tijeras' ");
-    playerSelection = elegir.toLowerCase();
+//Asignacion de eleccion "piedra" "papel" "tijeras"
+document.getElementById("piedra").addEventListener("click", function () {
+  if (playGame < 5) {
+    playerSelection = "piedra";
+    playRound();
   }
-  return playerSelection;
-}
+});
 
+document.getElementById("papel").addEventListener("click", function () {
+  if (playGame < 5) {
+    playerSelection = "papel";
+    playRound();
+  }
+});
+
+document.getElementById("tijeras").addEventListener("click", function () {
+  if (playGame < 5) {
+    playerSelection = "tijeras";
+    playRound();
+  }
+});
+
+//Eleccion al azar del computador
 function getComputerChoice() {
-  const elegir = ["piedra", "papel", "tijeras"];
-  const eleccionAleatoria = Math.floor(elegir.length * Math.random());
-  const computerSelection = elegir[eleccionAleatoria];
-  return computerSelection;
+  const options = ["piedra", "papel", "tijeras"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-  switch (true) {
-    case playerSelection === "piedra" && computerSelection === "tijeras":
-    case playerSelection === "papel" && computerSelection === "piedra":
-    case playerSelection === "tijeras" && computerSelection === "papel":
-      playerScore++;
-      return "Ganaste!";
+//Coomienzao del juego, y comparaciones de elecciones
+function playRound() {
+  const computerSelection = getComputerChoice();
+  let result = "";
 
-    case playerSelection === "tijeras" && computerSelection === "piedra":
-    case playerSelection === "piedra" && computerSelection === "papel":
-    case playerSelection === "papel" && computerSelection === "tijeras":
-      computerScore++;
-      return "Perdiste...";
-
-    case playerSelection === computerSelection:
-      return "Empate?!";
-  }
-}
-
-while (playGame < 3) {
-  let playerSelection = eleccion();
-
-  let computerSelection = getComputerChoice();
-
-  let rondas = playRound(playerSelection, computerSelection);
-  console.log(rondas);
-
-  console.log("Rondas ganadas: " + playerScore + " rondas perdidas: " + computerScore);
-
-  while (rondas === "empate") {
-    let playerSelection = eleccion();
-    console.log(playerSelection);
-
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection);
-
-    let rondas = playRound(playerSelection, computerSelection);
-    console.log(rondas);
-
-    console.log("Rondas ganadas: " + playerScore + " rondas perdidas: " + computerScore);
+  if (playerSelection === computerSelection) {
+    result = "Empate!";
+  } else if (
+    (playerSelection === "piedra" && computerSelection === "tijeras") ||
+    (playerSelection === "papel" && computerSelection === "piedra") ||
+    (playerSelection === "tijeras" && computerSelection === "papel")
+  ) {
+    result = "Ganaste!";
+    playerScore++;
+    playGame++;
+  } else {
+    result = "Perdiste...";
+    computerScore++;
+    playGame++;
   }
 
-  playGame++;
+  //Datos impresos en pantalla
+  const eleccionPlayer = `<span class="text">Player:</span><br>${playerSelection}<br><br>`;
+  document.getElementById("eleccionPlayer").innerHTML += eleccionPlayer;
+
+  const eleccionComputer = `<span class="text">Computer:</span><br>${computerSelection}<br><br>`;
+  document.getElementById("eleccionComputer").innerHTML += eleccionComputer;
+
+  const resultadoRonda = `<span class="text">Resultado</span><br>${result}<br><br>`;
+  document.getElementById("resultadoRonda").innerHTML += resultadoRonda;
+
+  if (playGame < 5) {
+    //Conttinuacion del juego mientras las rondas validas sean menor que 5
+  } else {
+    //Final del juego una vez las rondas sean igual a 5
+    if (playerScore > computerScore) {
+      const ganador = `El ganador es el jugador! ${playerScore} puntos a ${computerScore}, ¡Muy bien hecho!`;
+      document.getElementById("ganador").innerHTML += ganador;
+    } else {
+      const ganador = `El ganador es la máquina! ${computerScore} puntos a ${playerScore}, ¡Mejor suerte la próxima vez!`;
+      document.getElementById("ganador").innerHTML += ganador;
+    }
+
+    // Desactivar eventos de clic en los botones para no continuar imprimiendo resultados
+    document.getElementById("piedra").removeEventListener("click");
+    document.getElementById("papel").removeEventListener("click");
+    document.getElementById("tijeras").removeEventListener("click");
+  }
 }
